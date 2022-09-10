1e25170ed86e5f5c05a969a647836a9f5f1a747c
Nahuel <nahuelmath@gmail.com>
2022-09-09 22:16:55 -0400

    error1: no compra en orden correcto

--- tarea2/bolsa.c
+++ tarea2/bolsa.c
@@ -5,9 +5,6 @@
 
 #include "bolsa.h"
 
-// Declare aca sus variables globales
-// ...
-
 
 #define TRUE 1
 #define FALSE 0
@@ -22,35 +19,46 @@ char *nombreComprador;
 char* nombreVendedor;
 
 
+
 int vendo(int precio, char *vendedor, char *comprador) {
 
   pthread_mutex_lock(&m);
+  
   int local_status;
-  status = &local_status;
 
   if (mejor_precio == 0){ // Caso primer vendedor
     mejor_precio = precio;
+    local_status = 0;
+    status = &local_status;
   }
   else if (mejor_precio>precio){ // Llega nuevo vendedor con mejor precio, despierta al otro vendedor en espera
     mejor_precio = precio;
-    local_status = -1;
+    *status = -1;
+    local_status = 0;
+    status = &local_status;
     pthread_cond_broadcast(&c);
   }
+  else{
+    local_status = -1;
+    status = &local_status; // podria quitarse????????
+  }
 
+  // compradorCopia = &comprador;
   while(!*status) pthread_cond_wait(&c, &m);
   //pthread_mutex_unlock(&m);
 
   if (*status == 1){
-    strcpy(comprador, nombreComprador);
-    //pthread_mutex_lock(&m);
-    strcpy(nombreVendedor, vendedor);
+    comprador = nombreComprador; // Copiamos nombre comprador en comprador
+    nombreVendedor = vendedor;
+
     pthread_mutex_unlock(&m);
     return TRUE;
   }
   // Caso no se vende pues otro vendedor vende mas barato, se cambia local_status a 0 para q el vendedor con precio mas barato espero un comprador
   else{
-    //pthread_mutex_lock(&m);
-    local_status = 0;
+    *status = 0;
+    status = &local_status;
+
     pthread_mutex_unlock(&m);
     return FALSE;
   }
@@ -60,26 +68,33 @@ int vendo(int precio, char *vendedor, char *comprador) {
 
 int compro(char *comprador, char *vendedor) {
 
-  int mejor_precio_local;
   pthread_mutex_lock(&m);
 
-  strcpy(nombreComprador, comprador);
+  int mejor_precio_local;
   
   if (status == NULL){ // Caso no hay vendedores
     mejor_precio = 0;
+
     pthread_mutex_unlock(&m);
     return 0;
   } 
 
+  //strcpy(nombreComprador, comprador);
+  nombreComprador = comprador;
   *status = 1;
-  //pthread_mutex_unlock(&m);
+
+  pthread_mutex_unlock(&m);
   pthread_cond_broadcast(&c);
 
-  //pthread_mutex_lock(&m);  
-  strcpy(vendedor, nombreVendedor);
-  mejor_precio_local = mejor_precio;
+  //strcpy(vendedor, nombreVendedor);
+
+  vendedor = nombreVendedor;
+  pthread_mutex_lock(&m);
+  
   status = NULL;
+  mejor_precio_local = mejor_precio;
   mejor_precio = 0;
+
   pthread_mutex_unlock(&m);
 
   return mejor_precio_local;
